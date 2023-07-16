89ebae5f774a14c63de28512bc40f8012c786211
devbandeira <dev.bandeira@outlook.com>
2023-07-16 18:26:12 -0300

    Aula 0104 TypeScript Ferramentas necessárias

--- 0104-typescript-ferramentas.js
+++ 0104-typescript-ferramentas.js
@@ -1,13 +1,9 @@
-function somar(a: number, b: number){
-
-  return a + b;
-
+"use strict";
+function somar(a, b) {
+    return a + b;
 }
-
-console.log(somar(5,10));
-
-
+console.log(somar(5, 10));
 /* utilizando o comando tsc 0104-typescript-ferramentas.ts ele vai gerar o arquivo .JS automaticamente a partir do .TS
 Vou deixar automatico para compilar com qualquer mudança
 usando o "tsc -w"
-*/
+*/ 
